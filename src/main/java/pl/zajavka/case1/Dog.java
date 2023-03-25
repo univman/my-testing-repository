56fce2767b61c9d4e723bad220ed749faff8fc33
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-03-25 21:15:54 +0100

    Revert "Modification 2"
    This reverts commit 8ea7d07e10931d6cd3a98df8f8eace7eb70e9f37.

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,9 +1,5 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
-
-    @Override
-    public String whatIsYourName() {
-        return "My name is Dog";
-    }
+    private String name;
 }
